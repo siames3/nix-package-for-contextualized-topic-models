38528c6d6e590054e894578fed09cee46d6f192f
Jörg Thalheim <joerg@thalheim.io>
2021-01-26 16:05:26 +0100

    contextualized-topic-models: fix build

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-with import <nixpkgs> {};
+with import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/b749b2afa6f4ec5467c0e1cb3b86bb6a16056253.tar.gz") {};
 mkShell {
   propagatedBuildInputs = [
     (pkgs.python3.pkgs.callPackage ./contextualized-topic-models.nix {})
