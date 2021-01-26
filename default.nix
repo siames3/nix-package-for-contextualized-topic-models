012ffe37a5109e8ec4735431b580b117884cafee
Jörg Thalheim <joerg@thalheim.io>
2021-01-26 16:13:49 +0100

    add ipywidgets

--- default.nix
+++ default.nix
@@ -1,8 +1,10 @@
 with import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/b749b2afa6f4ec5467c0e1cb3b86bb6a16056253.tar.gz") {};
 mkShell {
-  propagatedBuildInputs = [
+  buildInputs = [
     (pkgs.python3.pkgs.callPackage ./contextualized-topic-models.nix {})
     (pkgs.python3.pkgs.callPackage ./fasttext.nix {})
     jupyter
+    python3
+    python3.pkgs.ipywidgets
   ];
 }
